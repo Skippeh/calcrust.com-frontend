aff369cec1e83aed24e95469e5f7aae2b7f046b8
Jonathan Lindahl <jonathanjl99@gmail.com>
2016-05-12 22:48:44 +0200

    Disable debug info (doesn't add angular specific classes like ng-scope to elements for example).

--- WebSite/js/module.js
+++ WebSite/js/module.js
@@ -1,11 +1,13 @@
-﻿angular.module("RustCalc", ["perfect_scrollbar", "ui.router", "ngDialog"]).config(["$locationProvider", ModuleConfig]);
+﻿angular.module("RustCalc", ["perfect_scrollbar", "ui.router", "ngDialog"]).config(["$locationProvider", "$compileProvider", ModuleConfig]);
 
-function ModuleConfig ($locationProvider)
+function ModuleConfig ($locationProvider, $compileProvider)
 {
     $.featherlight.defaults.closeOnClick = "anywhere";
     $.featherlight.defaults.closeIcon = "";
 
     $locationProvider.html5Mode(true);
+
+    $compileProvider.debugInfoEnabled(false);
 }
 
 console.log("Hello! If you want the item and blueprint data it's available as a global variable: RustData, or at /api-docs.");
