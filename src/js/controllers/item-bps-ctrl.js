17c1bc6b4afc284c2d794cfec274a57c32d0ca9a
Andrew Hughson <andrew@hughson.me>
2016-07-31 03:14:57 +1200

    Add item categories

--- src/js/controllers/item-bps-ctrl.js
+++ src/js/controllers/item-bps-ctrl.js
@@ -7,6 +7,20 @@ function ItemBPsController($scope, $rustData, $rootScope)
     $scope.items = Object.values($rustData.items);
     $scope.recipes = $rustData.recipes;
 
+    $scope.categories = [{
+        name: "all",
+        items: $scope.items
+    }].concat(_.map(_.groupBy($scope.items, "category"),
+        (items, name) => ({ name, items })
+    ));
+
+    $scope.selectCategory = function(category)
+    {
+        $scope.selectedCategory = category;
+    };
+
+    $scope.selectCategory($scope.categories[0]);
+
     $scope.itemActive = function (item)
     {
         return $scope.stateParams.id === item.id;
