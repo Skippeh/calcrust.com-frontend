aab9ff176c61fcd7373c1e4ea1e1ed6086e6ddc6
Andrew Hughson <andrew@hughson.me>
2016-07-31 01:49:07 +1200

    Remove separate list for recipes and show a recipe link on item in item list.

--- src/js/controllers/item-bps-ctrl.js
+++ src/js/controllers/item-bps-ctrl.js
@@ -1,72 +1,14 @@
-﻿angular.module("RustCalc").controller("ItemBPsController", ["$scope", "$filter", "$rustData", "$rootScope", "$state", ItemBPsController]);
+﻿angular.module("RustCalc").controller("ItemBPsController", ["$scope", "$rustData", "$rootScope", "$stateParams", ItemBPsController]);
 
-function ItemBPsController ($scope, $filter, $rustData, $rootScope, $state)
+function ItemBPsController($scope, $rustData, $rootScope, $stateParams)
 {
     $rootScope.page.titlePrefix = "Items & Blueprints";
 
-    $scope.blueprintFilter =
-    {
-        name: ""
-    };
-
-    $scope.itemFilter = {
-        name: ""
-    };
-
-    $scope.filteredRecipes = [];
-    $scope.filteredItems = [];
-
-    function applyRecipeFilter()
-    {
-        if (typeof $scope.rustData == "undefined")
-            return;
-
-        var blueprints = Object.values($scope.rustData.recipes);
-        blueprints = blueprints.filter($scope.filterBlueprints);
-        blueprints = $filter("orderBy")(blueprints, "output.item.name");
-
-        $scope.filteredRecipes = blueprints;
-        $(".blueprint-browser .browser-wrapper").scrollTop(0); // Reset scroll position to top of list.
-    }
-
-    function applyItemFilter ()
-    {
-        if (typeof $scope.rustData == "undefined")
-            return;
-
-        var items = $scope.itemsArray.filter($scope.filterItems);
-        items = $filter("orderBy")(items, "name");
-
-        $scope.filteredItems = items;
-        $(".item-browser .browser-wrapper").scrollTop(0); // Reset scroll position to top of list.
-    }
-
-    $scope.$watch("blueprintFilter", applyRecipeFilter, true);
-    $scope.$watch("itemFilter", applyItemFilter, true);
-
-    $scope.filterBlueprints = function (recipe)
-    {
-        return recipe.output.item.name.toLowerCase().indexOf($scope.blueprintFilter.name.toLowerCase()) != -1;
-    };
-    
-    $scope.filterItems = function (item)
-    {
-        return item.name.toLowerCase().indexOf($scope.itemFilter.name.toLowerCase()) != -1;
-    };
-    
-    $scope.bpActive = function (recipe)
-    {
-        return $scope.stateParams.id == recipe.output.item.id && ($state.includes("itembps.item.recipe") || $state.includes("itembps.item.recipes"));
-    }
+    $scope.items = Object.values($rustData.items);
+    $scope.recipes = $rustData.recipes;
 
     $scope.itemActive = function (item)
     {
-        return $scope.stateParams.id == item.id && $state.includes("itembps.item.info");
-    }
-
-    $scope.rustData = $rustData;
-    $scope.itemsArray = Object.keys($rustData.items).map(function (key) { return $rustData.items[key]; });
-
-    applyRecipeFilter();
-    applyItemFilter();
+        return $stateParams.id === item.id;
+    };
 }
