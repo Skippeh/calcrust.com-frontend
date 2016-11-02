b1d80cd5417f091718f5e6a6a651311c4a505123
Jonathan Lindahl <jonathanjl99@gmail.com>
2016-11-02 20:26:44 +0100

    Use new api method to get damageable items.

--- src/js/controllers/damage-tables-ctrl.js
+++ src/js/controllers/damage-tables-ctrl.js
@@ -14,26 +14,9 @@ function DamageTablesCtrl ($scope, $rootScope, $rustData, $q, $http)
         {
             $scope.loading = false;
             var damageInfos = response.data.data;
+            
             console.log(damageInfos);
-
-            var buildingNames = Object.keys(damageInfos[items[0].id].damages)
-                .filter(function(name)
-                {
-                    if (name.indexOf(":") !== -1)
-                        return name.indexOf(":twigs") !== -1;
-
-                    return true;
-                })
-                .map(function(name)
-                {
-                    var index = name.indexOf(":");
-
-                    if (index != -1)
-                        return name.substr(0, index);
-
-                    return name;
-                });
-            console.log(buildingNames);
+            $scope.damageInfos = damageInfos;
         }, function(error)
         {
             $scope.loading = false;
